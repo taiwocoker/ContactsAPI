c174ce45558702ea466d9d426838656b323b8245
taiwocoker <taiwofcoker@gmail.com>
2021-10-26 11:13:36 +0100

    build api and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
      resources :contacts
-      get 'contacts', to: 'contacts#index'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_182029) do
+ActiveRecord::Schema.define(version: 2021_10_26_095246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,6 @@ ActiveRecord::Schema.define(version: 2021_10_25_182029) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +31,4 @@ ActiveRecord::Schema.define(version: 2021_10_25_182029) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "contacts", "users"
 end
